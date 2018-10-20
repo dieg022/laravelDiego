f5787e61f303ab3de0ed4033e86433e1fa6470b5
Diego <diego.puya91@gmail.com>
2018-10-20 23:01:06 +0200

    Create providers ses Create providers Smtp

---
+++ app/Providers/SmtpProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class SmtpProvider extends ServiceProvider implements MailerProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
